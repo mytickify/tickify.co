8883892eecd7505658cf4aeb8577e5e017f67551
Anto Contreras <acontreras@sultans.co>
2025-11-05 21:04:22 -0500

    refactor(apollo-client): use constant for graphql endpoint uri

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -1,8 +1,12 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { SetContextLink } from '@apollo/client/link/context';
 import { HttpLink } from '@apollo/client/link/http';
+import { Route } from 'next';
+
+const APOLLO_URI: Route = '/graphql';
+
 const httpLink = new HttpLink({
-  uri: '/api/graphql',
+  uri: APOLLO_URI,
 });
 
 const authLink = new SetContextLink((prevContext, _) => {
