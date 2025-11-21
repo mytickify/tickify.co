5d3146d3f720c82b91a3e0a1cf494cb4be4bd455
Antonia Contreras <anto@pricom.cl>
2025-11-20 19:00:51 -0500

    refactor(graphql): add debug logs for schema loading
    Add console logs to help debug schema loading issues by showing current directory and files

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -5,10 +5,14 @@ import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import { resolvers } from './resolvers';
+import { readdirSync } from 'node:fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
+//console log current dirname
+console.log(__dirname)
+// log current files in dirname
+console.log(readdirSync(__dirname))
 // Load schema from schema.graphql file
 const typeDefs = await loadSchema(join(__dirname, './schema.graphql'), {
   loaders: [new GraphQLFileLoader()]
